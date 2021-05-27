b640481e307a51514842a9c888319cd3f7b1ec84
reiyon <nsprae@gmail.com>
2021-05-27 13:07:05 +0200

    Set up new project

---
+++ src/App/App.jsx
@@ -0,0 +1,11 @@
+import './app.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Nuanprae's Resume Web App</h2>
+      </header>
+    </div>
+  );
+}
