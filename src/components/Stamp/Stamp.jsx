9973493bd42c54ce9519f97ffbe77f960f72c998
reiyon <nsprae@gmail.com>
2021-06-01 10:51:24 +0200

    Style mobile view

--- src/components/Stamp/Stamp.jsx
+++ src/components/Stamp/Stamp.jsx
@@ -3,5 +3,28 @@ import classNamesHelper from 'classnames';
 import './stamp.css';
 
 export default function Stamp({ className }) {
-  return <section className={classNamesHelper('stamp', className)}></section>;
+  return (
+    <section className={classNamesHelper('stamp', className)}>
+      <h4 className="stamp__text">Sverige Brev</h4>
+      <section className="bear" alt="sleeping bear">
+        <div className="head">
+          <div className="head-copy"></div>
+          <div className="ear-left">
+            <div className="inner-ear"></div>
+          </div>
+          <div className="ear-right">
+            <div className="inner-ear"></div>
+          </div>
+          <div className="eye-left"></div>
+          <div className="eye-right"></div>
+          <div className="snout">
+            <div className="nose"></div>
+            <div className="nose-line"></div>
+            <div className="mouth"></div>
+          </div>
+        </div>
+      </section>
+      <h4 className="stamp__text">css</h4>
+    </section>
+  );
 }
